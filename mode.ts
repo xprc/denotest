790463c553ccfcdb04e87f5f2c11cd28cdea6896
TomsProject <44722363+TomsProject@users.noreply.github.com>
2022-08-28 13:30:59 +0800

    Update mode.ts

--- mode.ts
+++ mode.ts
@@ -1,40 +1,26 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/mod.ts";
+import { lookup } from "https://deno.land/x/media_types/mod.ts";
 
-async function handleRequest(request: Request): Promise<Response> {
-  const { pathname } = new URL(request.url);
+const BASE_PATH = "./public";
 
-  // This is how the server works:
-  // 1. A request comes in for a specific asset.
-  // 2. We read the asset from the file system.
-  // 3. We send the asset back to the client.
-
-  // Check if the request is for style.css.
-  if (pathname.startsWith("/style.css")) {
-    // Read the style.css file from the file system.
-    const file = await Deno.readFile("./style.css");
-    // Respond to the request with the style.css file.
-    return new Response(file, {
-      headers: {
-        "content-type": "text/css",
-      },
-    });
+const reqHandler = async (req: Request) => {
+  const filePath = BASE_PATH + new URL(req.url).pathname;
+  let fileSize;
+  try {
+    fileSize = (await Deno.stat(filePath)).size;
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      return new Response(null, { status: 404 });
+    }
+    return new Response(null, { status: 500 });
   }
-
-  return new Response(
-    `<html>
-      <head>
-        <link rel="stylesheet" href="style.css" />
-      </head>
-      <body>
-        <h1>Example</h1>
-      </body>
-    </html>`,
-    {
-      headers: {
-        "content-type": "text/html; charset=utf-8",
-      },
+  const body = (await Deno.open(filePath)).readable;
+  return new Response(body, {
+    headers: {
+      "content-length": fileSize.toString(),
+      "content-type": lookup(filePath) || "application/octet-stream",
     },
-  );
-}
+  });
+};
 
-serve(handleRequest);
+serve(reqHandler, { port: 80，443);
